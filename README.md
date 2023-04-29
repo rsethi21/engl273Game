2c57bf3580c193d0b1e89ff3342bc54f0b2ead8c
Rohan Sethi <rsethi1@luc.edu>
2023-04-29 00:26:20 -0500

    added movement and character class

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This is a game for my final project in Exploring Fiction based off of the gothic literature novels read (Dracula, Frankenstein, Haunting of Hill House)
 
 Game development tutorial: https://www.youtube.com/watch?v=Q-__8Xw9KTM
+
+Ideas: could you use NLP to allow users to input text and automatically generate an output from a NPC
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 # create pygame window (where game will be played on)
 WIDTH, HEIGHT = 1000, 500
@@ -19,16 +20,56 @@ doorImage = pygame.transform.scale(doorImage, (WIDTH, HEIGHT))
 dracula = pygame.image.load(os.path.join(imgDir, "dracula.png"))
 
 
+
+# create character class that can be used for multiple things like enemy and player
+class Character:
+
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.img = None
+        self.powers = []
+        self.turn = False
+
+    def draw(self, window):
+        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50), 10)
+
 # game loop function (allows interaction and changes in physics)
+
 def main():
     run = True
-    FPS = 60  # checks physics 60 frames per second
+    FPS = 120  # checks physics 60 frames per second
+    lives = 5
+    velocity = 10  # higher velocity for lower clockspeed; how many pixels moved per frame
+    main_font = pygame.font.SysFont("comicsans", 20)
+    item_font = pygame.font.SysFont("comicsans", 40)
+
+    box = Character(700, 400)  # dummy character create
+
     clock = pygame.time.Clock()  # keeps track of time of game
 
     # # want to do one-two things well in gaming inside function (might want different functions for different
     # choices and call in different conditionals in loop
     def redraw_window():
         WIN.blit(doorImage, (0, 0))  # pygame uses 0,0 as top left to add surface on screen
+
+        # # created text
+        lives_label = main_font.render(f"Lives Left: {lives}", True, (255, 0, 0))  # RGB format tuple
+        intro_text = item_font.render("Pick a Door", True, (255, 250, 250))
+        door_1 = item_font.render("1", True, (255, 250, 250))
+        door_2 = item_font.render("2", True, (255, 250, 250))
+        door_3 = item_font.render("3", True, (255, 250, 250))
+
+        # # plotting text
+        WIN.blit(lives_label, (0, 0))
+        WIN.blit(intro_text, (0.5*WIDTH-0.5*intro_text.get_width(), 0))
+        WIN.blit(door_1, (240, 40))
+        WIN.blit(door_2, (0.5*WIDTH-0.5*door_2.get_width(), 40)) # attempt at programmatic sense
+        WIN.blit(door_3, (740, 40))
+
+        box.draw(WIN)  # dummy character draw
+
         pygame.display.update()
 
     # # every time loop, check for events that have occurred
@@ -41,5 +82,14 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+            keys = pygame.key.get_pressed()  # dictionary of key presses and move in certain direction
+            if keys[pygame.K_LEFT] and box.x + velocity > 0:
+                box.x -= velocity
+            if keys[pygame.K_RIGHT] and box.x + velocity < WIDTH:
+                box.x += velocity
+            if keys[pygame.K_DOWN] and box.y + velocity < HEIGHT:
+                box.y += velocity
+            if keys[pygame.K_UP] and box.y + velocity > 0:
+                box.y -= velocity
 
 main()
