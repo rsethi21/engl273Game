b8ca9fd98ce0110086cc180ec7a1d90f8065b696
Rohan Sethi <rsethi1@luc.edu>
2023-04-29 11:11:45 -0500

    added character and movement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import os
 import time
 import random
+
 pygame.font.init()
 
 # create pygame window (where game will be played on)
@@ -18,7 +19,7 @@ doorImage = pygame.image.load(os.path.join(imgDir, "doors.jpg"))
 doorImage = pygame.transform.scale(doorImage, (WIDTH, HEIGHT))
 # # characters
 dracula = pygame.image.load(os.path.join(imgDir, "dracula.png"))
-
+dracula = pygame.transform.scale(dracula, (WIDTH/4, HEIGHT/1.8))
 
 
 # create character class that can be used for multiple things like enemy and player
@@ -33,19 +34,35 @@ class Character:
         self.turn = False
 
     def draw(self, window):
-        pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50), 10)
+        # pygame.draw.rect(window, (255, 0, 0), (self.x, self.y, 50, 50), 10)  # test object
+        window.blit(self.img, (self.x, self.y))
+
+
+class Player(Character):
+    def __int__(self, x, y, health=100):
+        super.__init__(x, y, health)
+
+
+class Enemy(Character):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.img = dracula
+        self.mask = pygame.mask.from_surface(self.img)  # creates mask to create collisions perfect since only pixel
+        # areas
+        self.max_health = health
+
 
 # game loop function (allows interaction and changes in physics)
 
 def main():
     run = True
-    FPS = 120  # checks physics 60 frames per second
+    FPS = 60  # checks physics 60 frames per second
     lives = 5
-    velocity = 10  # higher velocity for lower clockspeed; how many pixels moved per frame
+    velocity = 5  # higher velocity for lower clock-speed; how many pixels moved per frame
     main_font = pygame.font.SysFont("comicsans", 20)
     item_font = pygame.font.SysFont("comicsans", 40)
 
-    box = Character(700, 400)  # dummy character create
+    enemy = Enemy(WIDTH-dracula.get_width(), HEIGHT-dracula.get_height())  # dummy character create
 
     clock = pygame.time.Clock()  # keeps track of time of game
 
@@ -53,7 +70,6 @@ def main():
     # choices and call in different conditionals in loop
     def redraw_window():
         WIN.blit(doorImage, (0, 0))  # pygame uses 0,0 as top left to add surface on screen
-
         # # created text
         lives_label = main_font.render(f"Lives Left: {lives}", True, (255, 0, 0))  # RGB format tuple
         intro_text = item_font.render("Pick a Door", True, (255, 250, 250))
@@ -63,12 +79,12 @@ def main():
 
         # # plotting text
         WIN.blit(lives_label, (0, 0))
-        WIN.blit(intro_text, (0.5*WIDTH-0.5*intro_text.get_width(), 0))
+        WIN.blit(intro_text, (0.5 * WIDTH - 0.5 * intro_text.get_width(), 0))
         WIN.blit(door_1, (240, 40))
-        WIN.blit(door_2, (0.5*WIDTH-0.5*door_2.get_width(), 40)) # attempt at programmatic sense
+        WIN.blit(door_2, (0.5 * WIDTH - 0.5 * door_2.get_width(), 40))  # attempt at programmatic sense
         WIN.blit(door_3, (740, 40))
 
-        box.draw(WIN)  # dummy character draw
+        enemy.draw(WIN)  # dummy character draw everytime checking frames
 
         pygame.display.update()
 
@@ -77,19 +93,22 @@ def main():
         clock.tick(FPS)  # monitors when to update physics
         redraw_window()
 
+        # # # looks at all events except keys
         for event in pygame.event.get():
             # # # if close window then end game and close window
             if event.type == pygame.QUIT:
                 run = False
 
-            keys = pygame.key.get_pressed()  # dictionary of key presses and move in certain direction
-            if keys[pygame.K_LEFT] and box.x + velocity > 0:
-                box.x -= velocity
-            if keys[pygame.K_RIGHT] and box.x + velocity < WIDTH:
-                box.x += velocity
-            if keys[pygame.K_DOWN] and box.y + velocity < HEIGHT:
-                box.y += velocity
-            if keys[pygame.K_UP] and box.y + velocity > 0:
-                box.y -= velocity
+        # # # dictionary of keys pressed during the current frame; top left corner is always checked
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and enemy.x + velocity > 0:
+            enemy.x -= velocity
+        if keys[pygame.K_RIGHT] and enemy.x + velocity < WIDTH - enemy.img.get_width():
+            enemy.x += velocity
+        if keys[pygame.K_DOWN] and enemy.y + velocity < HEIGHT - enemy.img.get_height():
+            enemy.y += velocity
+        if keys[pygame.K_UP] and enemy.y + velocity > HEIGHT - enemy.img.get_height():
+            enemy.y -= velocity
+
 
 main()
