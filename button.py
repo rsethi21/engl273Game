7f392e36bc59f1018932d271863602fa024f042f
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-05-01 04:43:56 -0500

    Update button.py

--- button.py
+++ button.py
@@ -9,8 +9,11 @@ class Button:
     self.clicked = False
   
   def draw(self, WIN):
-    pos = pygame.mouse.get_pos()
-    
-      
     
     WIN.blit(self.image, (self.imageRect.x, self.imageRect.y))
+    
+    pos = pygame.mouse.get_pos()
+    if self.rectImage.collidepoint(pos):
+      pygame.draw.rect(WIN, pygame.Color(255, 255, 255, 128), pygame.Rect(self.imageRect.x, self.imageRect.y, self.imageRect.get_width(), self.imageRect.get_height()))
+      if pygame.mouseget_pressed()[0]:
+        self.clicked = True
