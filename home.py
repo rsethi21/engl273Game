588524561a0cd9039e0192097d3e0fb2c25f2432
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-05-01 12:57:36 -0500

    Update home.py

--- home.py
+++ home.py
@@ -44,18 +44,17 @@ three = None
 
 # game loop function (allows interaction and changes in physics)
 
-def main(lost=False):
+def main(finished=False):
     run = True
     FPS = 60  # checks physics 60 frames per second
-    lost = lost
-    lost_count = 0
+    finished = finished
+    finished_count = 0
     velocity_y = 5  # higher velocity for lower clock-speed; how many pixels moved per frame
     velocity_x = 8
     power_velocity = -10
     main_font = pygame.font.SysFont("comicsans", 20)
     item_font = pygame.font.SysFont("comicsans", 40)
-    lost_font = pygame.font.SysFont("comicsans", 100)
-    finished = False
+    end_font = pygame.font.SysFont("comicsans", 100)
 
 
     enemy = Enemy(WIDTH - dracula.get_width(), HEIGHT - dracula.get_height(), dracula)  # dummy character create
@@ -71,7 +70,7 @@ def main(lost=False):
     
     # # want to do one-two things well in gaming inside function (might want different functions for different
     # choices and call in different conditionals in loop
-    def redraw_window(loss):
+    def redraw_window(finish):
         # # created background
         WIN.blit(doorImage, (0, 0))  # pygame uses 0,0 as top left to add surface on screen
 
@@ -85,10 +84,10 @@ def main(lost=False):
         door_2.draw(WIN)
         door_3.draw(WIN)
         
-        if loss:
-            loss_label = lost_font.render("Game Over = (!", True, (255, 0, 0))
-            WIN.blit(loss_label,
-                     (0.5 * WIDTH - 0.5 * loss_label.get_width(), 0.5 * HEIGHT - 0.5 * loss_label.get_height()))
+        if finish:
+            finished_label = end_font.render("To be continued...?!?", True, (255, 0, 0))
+            WIN.blit(finished_label,
+                     (0.5 * WIDTH - 0.5 * finished_label.get_width(), 0.5 * HEIGHT - 0.5 * finished_label.get_height()))
 
         pygame.display.update()
 
@@ -99,16 +98,15 @@ def main(lost=False):
         if door_1.clicked == True and door_2.clicked == True and door_3.clicked == True:
             finished = True
         
-        redraw_window(loss=lost)
-        
+        redraw_window(finish=finished)
         
         if enemy2.health <= 0:
-            lost = True
-            lost_count += 1
+            finished = True
+            finished_count += 1
 
-        if lost:
+        if finished:
             # for more than 3 seconds
-            if lost_count >= FPS * 3:
+            if finish_count >= FPS * 3:
                 run = False
             else:
                 continue
@@ -127,9 +125,6 @@ def main(lost=False):
             enemy.x += velocity_x
         if keys[pygame.K_DOWN] and enemy.y + velocity_y <= HEIGHT - enemy.img.get_height() + velocity_y:
             enemy.y += velocity_y
-        if keys[pygame.K_UP] and enemy.y + velocity_y > HEIGHT - enemy.img.get_height() - velocity_y:
-            enemy.y -= velocity_y
-            # lives -= 1  # random test
         if keys[pygame.K_SPACE]:
             enemy.shoot()
         if enemy.currentpower is not None:
