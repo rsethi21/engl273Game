80b03493ab81384d8f95a5dd158b875f23bd0cd4
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-05-01 03:11:44 -0500

    Update dialogue.py

--- dialogue.py
+++ dialogue.py
@@ -2,6 +2,17 @@ import pygame
 
 def speech_bubble(WIN, text, pos):
   
-  pygame.font.init()
-  speech_font = pygame.font.SysFont("comicsans", 20)
-  
+  # creating bubble
+  pygame.font.init() # initialize font methods
+  speech_font = pygame.font.SysFont("comicsans", 20) # initialize font style
+  speech = speech_font.render(text, True, (255,255,255) # create text
+  text_wrap = speech.get_rect(midbottom=pos) # organize text as rect.
+  text_bubble = text_wrap.copy() # create background text bubble
+  text_bubble.inflate_ip(10, 10) # make the bubble bigger 
+  frame = text_bubble.copy() # create a bordering frame
+  frame.inflate_ip(4,4) # enlarge bordering frame
+                              
+  # draw
+  pygame.draw.rect(WIN, (255,255,255), frame) # draw frame
+  pygame.draw.rect(WIN, text_bubble, (0,0,0)) # superimpose box
+  WIN.blit(speech,texterap) # input text into box
