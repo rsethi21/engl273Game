6350b1e7fc79ebde0901e1b0306d0da2109c0eee
@rsethi <70125166+rsethi21@users.noreply.github.com>
2023-05-01 01:37:38 -0500

    Removed unecessary things

--- main.py
+++ main.py
@@ -22,9 +22,6 @@ bedroomImage = pygame.transform.scale(bedroomImage, (WIDTH, HEIGHT))
 doorImage = pygame.image.load(os.path.join(imgDir, "doors.jpg"))
 doorImage = pygame.transform.scale(doorImage, (WIDTH, HEIGHT))
 
-homeScreen = pygame.image.load(os.path.join(imgDir, "BG.jpg"))
-homeScreen = pygame.transform.scale(homeScreen, (WIDTH, HEIGHT))
-
 # # characters
 dracula = pygame.image.load(os.path.join(imgDir, "dracula.png"))
 dracula = pygame.transform.scale(dracula, (WIDTH / 4, HEIGHT / 1.8))
@@ -127,39 +124,3 @@ def main():
             enemy.shoot()
         if enemy.currentpower is not None:
             enemy.movePower(power_velocity, WIDTH, enemy2)
-
-
-def main_menu():
-    title_font = pygame.font.SysFont("comicsans", 50)
-    button_font = pygame.font.SysFont("comicsans", 20)
-    run = True
-    while run:
-        WIN.blit(homeScreen, (0,0))
-
-        title = title_font.render("Click play key to begin...", True, (255, 0, 0))
-        button = button_font.render("Play", True, (255, 255, 255))
-        button_dimensions = (button.get_width(), button.get_height())
-
-        WIN.blit(title, (0.5 * WIDTH - 0.5 * title.get_width(), 0.5 * HEIGHT - 0.5 * title.get_height()))
-
-        mouse = pygame.mouse.get_pos()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if 0.5 * WIDTH - 0.5 * button.get_width() <= mouse[0] <= 0.5 * WIDTH - 0.5 * button.get_width() + button_dimensions[0] and 0.5 * HEIGHT - 0.5 * button.get_height() + 40 <= mouse[1] <= 0.5 * HEIGHT - 0.5 * button.get_height() + 40 + button_dimensions[1]:
-                    main()
-        
-        if 0.5 * WIDTH - 0.5 * button.get_width() <= mouse[0] <= 0.5 * WIDTH - 0.5 * button.get_width() + button_dimensions[0] and 0.5 * HEIGHT - 0.5 * button.get_height() + 40 <= mouse[1] <= 0.5 * HEIGHT - 0.5 * button.get_height() + 40 + button_dimensions[1]:
-            pygame.draw.rect(WIN, (255, 0, 0), [0.5 * WIDTH - 0.5 * button.get_width(), 0.5 * HEIGHT - 0.5 * button.get_height() + 40, button_dimensions[0], button_dimensions[1]])
-        else:
-            pygame.draw.rect(WIN, (100, 0, 0), [0.5 * WIDTH - 0.5 * button.get_width(), 0.5 * HEIGHT - 0.5 * button.get_height() + 40, button_dimensions[0], button_dimensions[1]])
-        WIN.blit(button, (0.5 * WIDTH - 0.5 * button.get_width(), 0.5 * HEIGHT - 0.5 * button.get_height() + 40))
-
-        pygame.display.update()
-
-    pygame.quit()
-
-
-main_menu()
